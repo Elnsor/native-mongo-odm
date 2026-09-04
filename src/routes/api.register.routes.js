51b6411eaaa3c8e08a1801690d230a62d3a6d9bd
elnsor <elkasseh633@gmail.com>
2026-09-04 04:10:04 +0300

    (feature): add reate limit guard

--- src/routes/api.register.routes.js
+++ src/routes/api.register.routes.js
@@ -1,12 +1,25 @@
 import express from "express"
-import { register } from "../controllers/authRegister.contoller.js";
+import { register } from "../controllers/auth.register.controller.new.js";
+import {rateLimit} from "express-rate-limit"
 
 const registerRouter= express.Router();
+
+
+const authLimiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 5,
+    message: {
+        success: false,
+        message: 'Too many login attempts, please try again after 15 minutes'
+    },
+    standardHeaders: true,
+    legacyHeaders: false
+});
 
 registerRouter.get("/",(req,res)=>{
     res.status(200).send("Welcome To register Page 👌");
 });
 
-registerRouter.post("/",register);
+registerRouter.post("/",authLimiter,register);
 
 export default registerRouter
